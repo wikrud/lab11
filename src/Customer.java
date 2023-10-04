af511b79ef9a058bc3d81f4e5fdb829188307451
Jan Kowalski <jank.kowalski@pollub.edu.pl>
2023-10-04 09:42:55 +0200

    initial commit

--- src/Customer.java
+++ src/Customer.java
@@ -2,4 +2,8 @@ public class Customer {
  public void newCustomer(String name){
      System.out.println(name);
  }
+
+    public void updateCustomer(String name){
+        System.out.println(name);
+    }
 }
