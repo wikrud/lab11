430885c6f79906e80e6bf04b2fbed2b5ec2e9d70
Jan Kowalski <jank.kowalski@pollub.edu.pl>
2023-10-04 09:01:41 +0200

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         System.out.println("Hello world!");
     }
     public void aaa(){
-        System.out.println("xxx");
+        System.out.println("xxvcvx");
     }
 }
 //ctrl alt l - formatuje
