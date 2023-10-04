8b173381e8ae38a2f15d7b03f09b7d7f688293d0
Jan Kowalski <jank.kowalski@pollub.edu.pl>
2023-10-04 09:44:29 +0200

    feat-customer

--- src/Customer.java
+++ src/Customer.java
@@ -1,9 +1,10 @@
 public class Customer {
- public void newCustomer(String name){
-     System.out.println(name);
- }
+     public void newCustomer(String name){
+         System.out.println(name);
+     }
 
     public void updateCustomer(String name){
         System.out.println(name);
     }
 }
+//aa
